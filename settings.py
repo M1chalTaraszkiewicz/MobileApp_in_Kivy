bd3292fa3be6c5376fa8239062f163467650d710
K0tLecik <misiu12525@wp.pl>
2023-12-30 19:02:47 +0100

    Dodanie slidera zmieniającego wielkość czcionki w aplikacji.

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 from kivy.uix.screenmanager import Screen, SlideTransition
+from kivy.uix.slider import Slider
+from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
@@ -6,10 +8,10 @@ from kivy.uix.image import Image
 # zmienne globalne
 color = "blue"
 lang = "en"
-pl = ["Graj", "Ustawienia", "Wyjdź", "Wisielec", "Druga", "Trzecia", "Wróć", "Różowy", "Biały", "Niebieski", "Zielony", "Polski", "English"]
-en = ["Play", "Settings", "Quit", "Hangman", "Second", "Third", "Back", "Pink", "White", "Blue", "Green", "Polski", "English"]
+pl = ["Graj", "Ustawienia", "Wyjdź", "Wisielec", "Druga", "Trzecia", "Wróć", "Różowy", "Biały", "Niebieski", "Zielony", "Polski", "English", "Wielkość czcionki"]
+en = ["Play", "Settings", "Quit", "Hangman", "Second", "Third", "Back", "Pink", "White", "Blue", "Green", "Polski", "English", "Font size"]
 current_lang = en
-font_size = 12
+font_size = 15
 
 
 # funkcja zwracająca ścieżkę do tła aplikacji o wybranym kolorze
@@ -30,7 +32,7 @@ class Settings(Screen):
 
         # tworzenie przycisku do zmiany języka na angielski
         self.lang_button_en = Button(text=current_lang[12],
-                                     # font_size=font_size,
+                                     font_size=font_size,
                                      pos_hint={"center_x": 0.3, "y": 0.7},
                                      size_hint=(0.35, 0.1))
         self.lang_button_en.background_normal = get_button_path()
@@ -39,6 +41,7 @@ class Settings(Screen):
 
         # tworzenie przycisku do zmiany języka na polski
         self.lang_button_pl = Button(text=current_lang[11],
+                                     font_size=font_size,
                                      pos_hint={"center_x": 0.7, "y": 0.7},
                                      size_hint=(0.35, 0.1))
         self.lang_button_pl.background_normal = get_button_path()
@@ -77,6 +80,20 @@ class Settings(Screen):
         self.color_button_blue.bind(on_release=self.on_color_button_click)
         layout.add_widget(self.color_button_blue)
 
+        # tworzenie labelu pokazującego aktualną wielkość czcionki
+        self.font_size_label = Label(text=current_lang[13] + ":" + str(font_size),
+                                     font_size=font_size,
+                                     pos_hint={"center_x": 0.5, "y": 0.29},
+                                     size_hint=(0.5, 0.05))
+        layout.add_widget(self.font_size_label)
+
+        # tworzenie suwaka do zmiany wielkości czcionki w aplikacji
+        self.font_size_slider = Slider(min=12, max=22, value=15, step=1,
+                                       pos_hint={"center_x": 0.5, "y": 0.25},
+                                       size_hint=(0.8, 0.05))
+        self.font_size_slider.bind(value=self.on_slider_change)
+        layout.add_widget(self.font_size_slider)
+
         # tworzenie przycisku do powrotu do ekranu głównego
         self.back_button = Button(pos_hint={"center_x": 0.5, "y": 0.1},
                                   size_hint=(0.5, 0.1))
@@ -91,10 +108,39 @@ class Settings(Screen):
 
         self.add_widget(layout)
 
+    # funkcja suwaka zmieniająca wielkość czcionki we wszystkich ekranach
+    def on_slider_change(self, instance, value):
+        global font_size
+        font_size = value
+        self.back_button.font_size = value
+        self.color_button_pink.font_size = value
+        self.color_button_white.font_size = value
+        self.color_button_green.font_size = value
+        self.color_button_blue.font_size = value
+        self.lang_button_en.font_size = value
+        self.lang_button_pl.font_size = value
+        self.font_size_label.text = current_lang[13] + ": " + str(value)
+        self.font_size_label.font_size = value
+
+        # zmiana wielkości tekstów przycisków ekranu "main_menu"
+        main_menu = self.manager.get_screen('main_menu')
+        if main_menu:
+            main_menu.play_button.font_size = value
+            main_menu.settings_button.font_size = value
+            main_menu.quit_button.font_size = value
+
+        # zmiana wielkości tekstów przycisków ekranu "game_menu"
+        game_menu = self.manager.get_screen('game_menu')
+        if game_menu:
+            game_menu.back_button.font_size = value
+            game_menu.first_button.font_size = value
+            game_menu.second_button.font_size = value
+            game_menu.third_button.font_size = value
+
     # funkcja przycisków "lang_button_pl" i "ang_button_en" zmieniająca język wszystkich ekranów
     def on_language_button_click(self, instance):
         # pobieranie zmiennych globalnych
-        global current_lang, lang, en, pl
+        global current_lang, lang, en, pl, font_size
         # sprawdzenie, który przycisk został kliknięty
         if instance.text == "Polski":
             current_lang = pl
@@ -110,6 +156,7 @@ class Settings(Screen):
         self.color_button_white.text = current_lang[8]
         self.color_button_green.text = current_lang[10]
         self.color_button_blue.text = current_lang[9]
+        self.font_size_label.text = current_lang[13] + ": " + str(font_size)
 
         # zmiana tekstów przycisków ekranu "main_menu"
         main_menu = self.manager.get_screen('main_menu')
